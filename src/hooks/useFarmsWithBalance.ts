b6ad585b4328a6a81d268e1630128214de378fcb
TiboNo <75533136+TiboNo@users.noreply.github.com>
2021-02-19 15:44:11 +0100

    Update useFarmsWithBalance.ts

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingEgg',
+        name: 'pendingPomme',
         params: [farm.pid, account],
       }))
 
