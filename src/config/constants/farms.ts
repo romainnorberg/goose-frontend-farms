aa608db1e2025e5d120ae683e9f5420da978bb89
TiboNo <75533136+TiboNo@users.noreply.github.com>
2021-02-19 16:25:06 +0100

    Update farms.ts

--- src/config/constants/farms.ts
+++ src/config/constants/farms.ts
@@ -20,22 +20,6 @@ const farms: FarmConfig[] = [
   },
   {
     pid: 1,
-    risk: 5,
-    lpSymbol: 'EGG-BNB LP',
-    lpAddresses: {
-      97: '',
-      56: '0xd1b59d11316e87c3a0a069e80f590ba35cd8d8d3',
-    },
-    tokenSymbol: 'EGG',
-    tokenAddresses: {
-      97: '',
-      56: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
-    },
-    quoteTokenSymbol: QuoteToken.BNB,
-    quoteTokenAdresses: contracts.wbnb,
-  },
-  {
-    pid: 1,
     risk: 1,
     lpSymbol: 'USDT-BUSD LP',
     lpAddresses: {
